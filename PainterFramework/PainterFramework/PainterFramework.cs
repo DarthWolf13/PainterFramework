70f6f08b3422d7551710c1d5fb75db295204981f
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-03-05 13:57:13 +0100

    Setup A and B

---
+++ PainterFramework/PainterFramework/PainterFramework.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PainterFramework
+{
+    public class PainterFramework : GameEnvironment
+    {
+        public PainterFramework()
+        {
+            Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            gameStateManager.AddGameState("playingState", new PainterGameWorld());
+            gameStateManager.SwitchTo("playingState");
+        }
+    }
+}
---
+++ PainterFramework/PainterFramework/PainterGameWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PainterFramework
+{
+    class PainterGameWorld : GameObjectList
+    {
+        public PainterGameWorld()
+        {
+            this.Add(new SpriteGameObject("spr_background"));
+        }
+    }
+}
